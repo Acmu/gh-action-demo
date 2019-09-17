553a2ced2470584d2610af6306cbbceb1dad2fc8
明远 <zhengmingyuan@sensorsdata.cn>
2019-09-17 21:45:46 +0800

    add cra code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Night is so cool~ <strong>s1124yy@gmail.com</strong>
+        </p>
+        <a
+          className="App-link"
+          href="https://gitee.com/wen98y/projects"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Night
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
