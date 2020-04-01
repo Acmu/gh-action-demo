8b71a00ffaaa6a5c81d2db6e3d90f537da00c537
Acmu <s1124yy@gmail.com>
2020-04-01 07:47:33 +0000

    Deploying to gh-pages from master d17ffc855e3f6bcabe210be6b25055f0df63f8f3

---
+++ precache-manifest.9f66299e6f5febcae14eeb7176acd89f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f34b9b0228aaa170673becac0f87855a",
+    "url": "/gh-action-demo/index.html"
+  },
+  {
+    "revision": "68d8b18c8a6fffd0cf6e",
+    "url": "/gh-action-demo/static/css/2.5b4d90a6.chunk.css"
+  },
+  {
+    "revision": "f80989ad7fc9aabf5727",
+    "url": "/gh-action-demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "68d8b18c8a6fffd0cf6e",
+    "url": "/gh-action-demo/static/js/2.4d496c9d.chunk.js"
+  },
+  {
+    "revision": "f80989ad7fc9aabf5727",
+    "url": "/gh-action-demo/static/js/main.4e60827d.chunk.js"
+  },
+  {
+    "revision": "e545f8737a69840095a9",
+    "url": "/gh-action-demo/static/js/runtime~main.1d90814f.js"
+  }
+]);
